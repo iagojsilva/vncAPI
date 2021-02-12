c1dab951c93bb62a8e1aa28a2940da7dfa1ade7a
iaguinzz <iagosilva7contato@gmail.com>
2021-02-12 17:03:00 -0300

    deploy

--- API/API/settings.py
+++ API/API/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = ['192.168.1.34', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
